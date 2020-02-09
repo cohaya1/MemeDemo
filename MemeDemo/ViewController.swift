d7ec889774595c23c11ad4cd669b1f970c39a3de
cohaya1 <chika.ohaya@gmail.com>
2020-02-09 01:20:14 -0500

    Initial Commit

---
+++ MemeDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeDemo
+//
+//  Created by Makaveli Ohaya on 2/9/20.
+//  Copyright © 2020 Ohaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
